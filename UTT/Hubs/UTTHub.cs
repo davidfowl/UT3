6b0440cd6b15ba3ba605e104e6bea3570ac10e86
David Fowler <davidfowl@gmail.com>
2018-05-04 13:11:34 -0700

    Join Game working

--- UTT/Hubs/UTTHub.cs
+++ UTT/Hubs/UTTHub.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
@@ -10,6 +13,7 @@ namespace UTT
 
         public override Task OnConnectedAsync()
         {
+            Clients.All.SendAsync("GameUpdated", Game.GetGames());
             return Clients.All.SendAsync("UserJoined", UserName);
         }
 
@@ -18,9 +22,53 @@ namespace UTT
             return Clients.All.SendAsync("LobbyMessage", UserName, message);
         }
 
+        public Task CreateGame()
+        {
+            var game = Game.CreateGame(UserName);
+            return Clients.All.SendAsync("GameUpdated", Game.GetGames());
+        }
+
+        public Task JoinGame(int id)
+        {
+            var game = Game.JoinGame(id, UserName);
+            return Clients.All.SendAsync("GameUpdated", Game.GetGames());
+        }
+
         public override Task OnDisconnectedAsync(Exception exception)
         {
             return Clients.All.SendAsync("UserLeft", UserName);
         }
+    }
+
+
+    public class Game
+    {
+        private static ConcurrentDictionary<int, Game> _games = new ConcurrentDictionary<int, Game>();
+        private static int _id;
+
+        public int Id { get; set; }
+        public string Player1 { get; set; }
+        public string Player2 { get; set; }
+        public string Status { get; set; }
+
+        public static Game CreateGame(string player)
+        {
+            var id = Interlocked.Increment(ref _id);
+            var game = new Game { Id = id, Player1 = player, Status = "Waiting" };
+            _games.TryAdd(id, game);
+            return game;
+        }
+
+        public static Game JoinGame(int id, string userName)
+        {
+            if (_games.TryGetValue(id, out var game) && game.Player2 == null)
+            {
+                game.Player2 = userName;
+                game.Status = "Playing";
+            }
+            return game;
+        }
+
+        public static IEnumerable<Game> GetGames() => _games.Values;
     }
 }
