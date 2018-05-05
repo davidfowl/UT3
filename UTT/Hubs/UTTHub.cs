c4df2f41590e89e4e6d22dfa2fcff1b0c3ede98a
David Fowler <davidfowl@gmail.com>
2018-05-05 01:33:26 -0700

    Small nits

--- UTT/Hubs/UTTHub.cs
+++ UTT/Hubs/UTTHub.cs
@@ -58,11 +58,16 @@ namespace UTT
 
     public class User
     {
-        private static ConcurrentDictionary<string, string> _users = new ConcurrentDictionary<string, string>();
+        public string Name { get; set; }
+
+        private static ConcurrentDictionary<string, User> _users = new ConcurrentDictionary<string, User>();
 
         public static void AddUser(string user)
         {
-            _users.TryAdd(user, user);
+            _users.TryAdd(user, new User
+            {
+                Name = user
+            });
         }
 
         public static void Remove(string user)
@@ -70,7 +75,7 @@ namespace UTT
             _users.TryRemove(user, out _);
         }
 
-        public static IEnumerable<string> GetUsers() => _users.Values;
+        public static IEnumerable<User> GetUsers() => _users.Values;
     }
 
     public class Game
