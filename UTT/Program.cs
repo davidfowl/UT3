d7cfbfb0141273728d5534f56e642eebb14cf061
David Fowler <davidfowl@gmail.com>
2018-05-11 00:14:31 -0700

    Logging service things

--- UTT/Program.cs
+++ UTT/Program.cs
@@ -19,6 +19,10 @@ namespace UTT
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddFilter("Microsoft.Azure.SignalR", LogLevel.Trace);
+                })
                 .UseStartup<Startup>();
     }
 }
